3c92194a2d4a6d3e2a580c3bef5469cc7db19c69
HASHIMOTO Wataru <waterloochant@gmail.com>
2016-03-20 22:52:38 +0900

    Add xcode project template.

---
+++ sketch-expo/sketch-expo/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  sketch-expo
+//
+//  Created by HASHIMOTO Wataru on 3/17/16.
+//  Copyright © 2016 HASHIMOTO Wataru. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
